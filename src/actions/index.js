d6c8fe12f7212ced2cb0897e4720ba39ee724190
averagealloy <m.mctighe10@gmail.com>
2020-04-24 16:25:25 -0400

    putting it back

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => {
-    return function (dispatch, getState) {
+    return async function (dispatch, getState) {
 
-        const promise = jsonPlaceholder.get('/posts')
+        const response = await jsonPlaceholder.get('/posts')
         
-        return {
-            type: 'FETCH_POSTS',
-            payload: promise
-        }
+       dispatch({type: 'FETCH_POSTS', payload: response})
     }
 }
 
