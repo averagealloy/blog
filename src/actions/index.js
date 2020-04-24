df7577b7f9551587c2d78ddb5dc9723934b2d98b
averagealloy <m.mctighe10@gmail.com>
2020-04-24 16:34:01 -0400

    missing something in my lock file (redux-thunk)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = () => {
-    return async function (dispatch, getState) {
-
+export const fetchPosts = () => 
+    async dispatch => {
         const response = await jsonPlaceholder.get('/posts')
-        
-       dispatch({type: 'FETCH_POSTS', payload: response})
+        dispatch({type: 'FETCH_POSTS', payload: response})
     }
-}
+
 
